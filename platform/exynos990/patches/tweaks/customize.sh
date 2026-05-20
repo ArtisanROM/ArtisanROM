f92df8b532f1c279c6b2d21732b7affa91533894
Android-Artisan <romartisan2025@gmail.com>
2026-05-20 09:34:58 +0200

    platform: exynos990: patches: tweaks: fixup for r8s

--- platform/exynos990/patches/tweaks/customize.sh
+++ platform/exynos990/patches/tweaks/customize.sh
@@ -98,9 +98,8 @@ SET_PROP "product" "bluetooth.profile.pbap.server.enabled" "true"
 SET_PROP "product" "bluetooth.profile.sap.server.enabled" "true"
 SET_PROP "product" "bluetooth.profile.ccp.server.enabled" "false"
 SET_PROP "product" "bluetooth.profile.vcp.controller.enabled" "false"
-
-if [[ "$TARGET_CODENAME" == "r8s" ]]; then
-    ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.bt.apex" 0 0 644 "u:object_r:system_file:s0"
+if [[ "$TARGET_CODENAME" = "r8s" ]]; then
+ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.bt.apex" 0 0 644 "u:object_r:system_file:s0"
 fi
 LOG_STEP_OUT
 
