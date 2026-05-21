1c25c119f54998a9faa3d901fd9185ffc8eff6c7
xfwdrev <frignacio16@gmail.com>
2026-05-21 14:04:32 +0200

    platform: exynos990: upstream

--- platform/exynos990/patches/blobs/customize.sh
+++ platform/exynos990/patches/blobs/customize.sh
@@ -61,8 +61,11 @@ ADD_TO_WORK_DIR "r9sxxx" "product" "priv-app/HotwordEnrollmentOKGoogleEx3CORTEXM
 ADD_TO_WORK_DIR "r9sxxx" "product" "priv-app/HotwordEnrollmentXGoogleEx3CORTEXM4/HotwordEnrollmentXGoogleEx3CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
-if [[ "$TARGET_CODENAME" != "r8s" ]]; then
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/android.hardware.sensor.hifi_sensors.xml" 0 0 644 "u:object_r:system_file:s0"
+
+if [[ "$TARGET_CODENAME" != "r8s" ]]; then
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.app.ledbackcover.xml" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/LedBackCoverAppHubble/LedBackCoverAppHubble.apk" 0 0 644 "u:object_r:system_file:s0"
 fi
 
 LOG_STEP_IN "- Adding 32-bit WFD blobs"
