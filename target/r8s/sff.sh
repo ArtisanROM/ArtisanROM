55228b8f6bd079bf91922b429e5f1948b5c66355
Android-Artisan <romartisan2025@gmail.com>
2026-06-01 05:35:02 +0200

    target: r8s: fixup/upstream

--- target/r8s/sff.sh
+++ target/r8s/sff.sh
@@ -6,5 +6,11 @@
 # Enable AOD live clock
 SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
 
+# Set default HFR Refresh Rate
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_DEFAULT_REFRESH_RATE=120
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
 # Enable extra brightness feature
 SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
