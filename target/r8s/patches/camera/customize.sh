21098896e4cc72345d2d79f1fa8a9899edc92cc4
Android-Artisan <romartisan2025@gmail.com>
2026-07-18 21:15:49 +0200

    target: r8s: patches: cam: fixup
    Signed-off-by: Android-Artisan <romartisan2025@gmail.com>

--- target/r8s/patches/camera/customize.sh
+++ target/r8s/patches/camera/customize.sh
@@ -3,6 +3,3 @@ SKIPUNZIP=1
 ADD_TO_WORK_DIR "$MODPATH" "system" "."
 
 DECODE_APK "system" "system/priv-app/SamsungCamera/SamsungCamera.apk"
-
-APPLY_PATCH "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" \
-    "$MODPATH/smali/system/priv-app/SamsungCamera/SamsungCamera.apk/0001-Fix-camera-crash-make-LlHdrNodeChainComposer.configure-a-no-op.patch"
