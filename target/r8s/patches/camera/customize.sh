71a00da82d4a8b99e36e929b53ed7a9b4684f20b
Android-Artisan <romartisan2025@gmail.com>
2026-08-29 16:47:30 +0200

    bombanuke?, part2 the revenge?
    Signed-off-by: Android-Artisan <romartisan2025@gmail.com>

--- target/r8s/patches/camera/customize.sh
+++
@@ -1,5 +0,0 @@
-SKIPUNZIP=1
-
-ADD_TO_WORK_DIR "$MODPATH" "system" "."
-
-DECODE_APK "system" "system/priv-app/SamsungCamera/SamsungCamera.apk"
--- target/r8s/patches/fod/customize.sh
+++
@@ -1,3 +0,0 @@
-SKIPUNZIP=1
-
-DECODE_APK "system" "system/framework/services.jar"
